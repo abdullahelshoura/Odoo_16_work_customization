37ad0517929473a11d998d9727d88ed9004f3de0
abdullahelshoura <36606092+abdullahelshoura@users.noreply.github.com>
2024-01-24 15:58:47 +0200

    Create a new report from scratch for invoice report

---
+++ saudi_vat_invoice/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'Saudi Vat Invoice',
+    'version': '1.0',
+    'category': 'account',
+    'summary': 'Print Saudi Vat Invoice',
+    'description': """
+    this module for adding new report in Invoice"
+    """,
+    'author': "",
+    'website': "",
+    'depends': ['base','account'],
+    'license': 'AGPL-3',
+    'data': [
+            'report/saudi_vat_invoice_report.xml',
+            ],
+
+    'demo': [],
+}
